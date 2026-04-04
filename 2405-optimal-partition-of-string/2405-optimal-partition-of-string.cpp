0f0a8dcaca46c5562e5220a6186d258a7bc4de46
Mohit Kumar Thakur <149611804+Mohit-Kumar-Thakur@users.noreply.github.com>
2026-04-04 21:38:08 +0530

    04-04-2026 2405-optimal-partition-of-string 12 ms (65.96%) 13.2 MB (63.1%)

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int partitionString(string s) {
+        vector<bool> seen(26,false);
+        int count = 1;
+        for(char c : s){
+            if(seen[c - 'a']){
+                count++;
+                fill(seen.begin(),seen.end(),false);
+            }
+            seen[c-'a'] = true;
+        }
+        return count;
+    }
+};

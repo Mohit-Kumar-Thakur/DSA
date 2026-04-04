dac40d47580913c826212b932b8b742ddff35cbd
Mohit Kumar Thakur <149611804+Mohit-Kumar-Thakur@users.noreply.github.com>
2026-04-04 21:27:01 +0530

    Time: 0 ms (100%), Space: 12.3 MB (8.63%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        string res = "";
+        for(char c : s){
+            res.push_back(c);
+            if(res.size()>=part.size() && res.substr(res.size()-part.size())==part){
+                res.erase(res.size()-part.size());
+            }
+        }
+        return res;
+    }
+};
